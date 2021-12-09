07b94b2b28b232b2d9cb2f55f0273b1108433c0e
Yalkann <lanneau.t@free.fr>
2021-12-09 17:27:15 +0100

    ajout de la création des nouvelles tables (1.3)

--- data/v1_createDB.sql
+++ data/v1_createDB.sql
@@ -1,31 +1,83 @@
 -- TODO 1.3 : Créer les tables manquantes et modifier celles ci-dessous
-create table LesRepresentations (
-    noSpec integer not null,
-    nomSpec varchar(50) not null,
-    dateRep date not null,
-    promoRep decimal (4,2) not null,
-    prixBaseSpec decimal (6,2) not null,
-    prixRep decimal (6,2) not null,
-    constraint pk_rep_noSpec_dateRep primary key (noSpec, dateRep),
-    constraint ck_rep_noSpec check (noSpec > 0),
-    constraint ck_spec_prixBaseSpec check (prixBaseSpec >= 0),
-    constraint ck_spec_prixBaseSpec check (prixRep >= 0),
-    constraint ck_rep_promoRep check (promoRep >= 0 and promoRep <=1)
+create table LesTaux (
+    typeZone varchar (50),
+    tauxZone decimal (4,2) not null,
+    constraint pk_Tau_typeZone primary key (typeZone),
+    constraint ck_Tau_tauxZone check (tauxZone >= 0 and tauxZone <=1),
+    constraint ck_Tau_cat check (typeZone in ('Orchestre', 'Balcon', 'Poulailler'))
+);
+
+create table LesZones (
+    noZone integer,
+    typeZone varchar(50),
+    constraint pk_Zon_noZ primary key (noZone),
+    constraint ck_Zon_noZone check (noZone > 0),
+    constraint fk_Zon_typeZone foreign key (typeZone) references LesTaux(typeZone)
+        on update cascade
+        on delete cascade
 );
 
 create table LesPlaces (
     noPlace integer,
     noRang integer,
-    noZone integer not null,
-    catZone varchar (50) not null,
-    tauxZone decimal (4,2) not null,
-    constraint pk_pl_noP_noR primary key (noZone, noPlace, noRang),
-    constraint ck_pl_noP check (noPlace > 0),
-    constraint ck_pl_noR check (noRang > 0),
-    constraint ck_pl_noZone check (noZone > 0),
-    constraint ck_pl_tauxZone check (tauxZone >= 0),
-    constraint ck_pl_cat check (catZone in ('orchestre', 'balcon', 'poulailler'))
+    noZone integer,
+    constraint pk_pla_noP_noR primary key (noPlace, noRang),
+    constraint ck_pla_noP check (noPlace > 0),
+    constraint ck_pla_noR check (noRang > 0),
+    constraint fk_pla_noZ foreign key (noZone) references LesZones(noZone)
+        on update cascade
+        on delete cascade
 );
+
+create table LesInformations (
+    noSpec integer,
+    nomSpec varchar (80),
+    prixBaseSpec decimal(6,2),
+    constraint pk_Inf_noSpec primary key (noSpec),
+    constraint ck_Inf_PBS check (prixBaseSpec >= 0)
+);
+
+create table LesRepresentations (
+    dateRep date not null,
+    promoRep decimal (4,2) not null,
+    noSpec integer not null,
+    constraint pk_rep_noSpec_dateRep primary key (dateRep),
+    constraint ck_rep_promoRep check (promoRep >= 0 and promoRep <=1),
+    constraint fk_rep_noSpec foreign key (noSpec) references LesInformations(noSpec)
+        on update cascade
+        on delete cascade
+);
+
+create table LesDossiers (
+    noDos integer,
+    constraint pk_Dos_noD primary key (noDos)
+);
+
+create table LesSituations (
+    situation varchar (50),
+    promoSit decimal (4,2),
+    constraint pk_Sit_sit primary key (situation),
+    constraint ck_Sit_pro check (promoSit >= 0 and promoSit <=1),
+    constraint pk_Sit_sit check (situation in ('adhérent', 'étudiant', 'scolaire', 'militaire', 'sénior', 'sans réduction'))
+);
+
+create table LesTickets (
+    noTic integer,
+    dateAchat date not null,
+    situation varchar(50) not null,
+    dateRep date not null,
+    noPlace integer not null,
+    noRang integer not null,
+    noDos integer not null,
+    constraint pk_Tic_noT primary key (noTic),
+    constraint fk_Tic_sit foreign key (situation) references LesSituations(situation),
+    constraint fk_Tic_daR foreign key (dateRep) references LesRepresentations(dateRep),
+    constraint fk_Tic_noP foreign key (noPlace, noRang) references LesPlaces(noPlace, noRang),
+    constraint fk_Tic_noD foreign key (noDos) references LesDossiers(noDos)
+        on update cascade
+        on delete cascade
+);
+
 -- TODO 1.4 : Créer une vue LesRepresentations ajoutant le nombre de places disponible et d'autres possibles attributs calculés.
--- TODO 1.5 : Créer une vue  avec le noDos et le montant total correspondant.
+
 -- TODO 3.3 : Ajouter les éléments nécessaires pour créer le trigger (attention, syntaxe SQLite différent qu'Oracle)
