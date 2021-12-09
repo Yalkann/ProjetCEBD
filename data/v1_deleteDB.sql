c04560b14d9358be910c247a920760d054fa8274
Yalkann <lanneau.t@free.fr>
2021-12-09 16:59:14 +0100

    changement suppressions de table (1.3)

--- data/v1_deleteDB.sql
+++ data/v1_deleteDB.sql
@@ -1,18 +1,10 @@
 -- TODO 1.3 : Détruire les tables manquantes et modifier celles ci-dessous
 DROP TABLE IF EXISTS LesTickets;
-DROP TABLE IF EXISTS LesSituations;
-DROP TABLE IF EXISTS LesDossiers;
-DROP TABLE IF EXISTS LesPersonnes;
-DROP TABLE IF EXISTS LesTickets;
 
 DROP TABLE IF EXISTS LesSituations;
 DROP TABLE IF EXISTS LesDossiers;
 DROP TABLE IF EXISTS LesPlaces;
 DROP TABLE IF EXISTS LesRepresentations;
-DROP TABLE IF EXISTS LesInformations;
-DROP TABLE IF EXISTS LesPlaces;
-DROP TABLE IF EXISTS LesZones;
-DROP TABLE IF EXISTS LesTaux;
 
 DROP TABLE IF EXISTS LesZones;
 DROP TABLE IF EXISTS LesInformations;
