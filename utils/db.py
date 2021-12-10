e555d4574c9489a9c40f836664582a5af010d9c3
Exioloz <emilien.maillard.simon@gmail.com>
2021-12-10 05:04:11 +0100

    Ajout de updateDBfileTrigger() pour la séparation de query trigger (3.3)

--- utils/db.py
+++ utils/db.py
@@ -15,3 +15,17 @@ def updateDBfile(data:sqlite3.Connection, file):
     cursor = data.cursor()
     for query in sqlQueries:
         cursor.execute(query)
+
+#3.3 : Ajouter les éléments nécessaires pour créer le trigger (attention, syntaxe SQLite différent qu'Oracle)
+def updateDBfileTrigger(data:sqlite3.Connection, file):
+
+    # Lecture du fichier et placement des requêtes dans un tableau
+    createFile = open(file, 'r')
+    createSql = createFile.read()
+    createFile.close()
+    sqlQueries = createSql.split("/")
+
+    # Exécution de toutes les requêtes du tableau
+    cursor = data.cursor()
+    for query in sqlQueries:
+        cursor.execute(query)
