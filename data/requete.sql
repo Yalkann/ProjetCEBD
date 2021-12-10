c2c869e5b6fd212997b522a8f7869a2a970e5314
Exioloz <emilien.maillard.simon@gmail.com>
2021-12-10 07:43:36 +0100

    Ajout de requetes sql dans requete.sql

--- data/requete.sql
+++ data/requete.sql
@@ -5,3 +5,20 @@ SELECT * FROM LesPlaces;
 SELECT * FROM LesRepresentations;
 SELECT * FROM LesDossiers;
 SELECT * FROM LesTickets;
+SELECT * FROM LesTickets
+JOIN LesPlaces USING (noPlace, noRang)
+JOIN LesZones USING (noZon)
+WHERE typeZone = 'Balcon';
+SELECT * FROM LesTickets
+JOIN LesReprentations USING (dateRep)
+JOIN LesInformations USING (noSpec)
+WHERE nomSpec = 'How to be a Parisian ?';
+SELECT * FROM LesTickets
+JOIN LesDossiers USING(noDos)
+WHERE dateCrea = '26/11/2008 20:18:20';
+SELECT LesDossier.noDos FROM LesTaux
+JOIN LesZones USING (typeZone)
+JOIN LesPlaces USING (noZone)
+JOIN LesTickets USING (noPlace, noRang)
+JOIN LesDossier USING (noDos)
+WHERE typeZone = 'orchestre';
