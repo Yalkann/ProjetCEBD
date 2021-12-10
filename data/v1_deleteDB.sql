f7222078730c95c0a2f2819386d2b972f3aa538c
Exioloz <emilien.maillard.simon@gmail.com>
2021-12-10 05:02:27 +0100

    Ajout de commentaires sur les dépendance (v1_deleteDB.sql)

--- data/v1_deleteDB.sql
+++ data/v1_deleteDB.sql
@@ -1,11 +1,21 @@
+-- Views
 DROP VIEW IF EXISTS LesDossiersView;
 DROP VIEW IF EXISTS LesTicketsView;
 DROP VIEW IF EXISTS LesRepresentationsView;
+
+-- Tables sans dépendance
 DROP TABLE IF EXISTS LesTickets;
+
+-- Tables en dépendance uniquement avec la table LesTickets
 DROP TABLE IF EXISTS LesSituations;
 DROP TABLE IF EXISTS LesDossiers;
 DROP TABLE IF EXISTS LesPlaces;
 DROP TABLE IF EXISTS LesRepresentations;
-DROP TABLE IF EXISTS LesZones;
+
+-- Table en dépendance avec LesRepresentations
 DROP TABLE IF EXISTS LesInformations;
+-- Table en dépendance avec LesPlaces
+DROP TABLE IF EXISTS LesZones;
+
+-- Table en dépendance avec LesZones
 DROP TABLE IF EXISTS LesTaux;
